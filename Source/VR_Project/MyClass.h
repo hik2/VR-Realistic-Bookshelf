72d0e513b6047325c549682daf14221eea893d61
Jane Kim <hik2@students.waikato.ac.nz>
2020-02-20 10:11:04 +1300

    Added Unreal files

---
+++ Source/VR_Project/MyClass.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class VR_PROJECT_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
